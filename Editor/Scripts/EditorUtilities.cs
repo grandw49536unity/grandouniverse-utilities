34acddde55ce4bc6bf2ed90a75a4b969c1121cc6
kclee <grando93290@gmail.com>
2024-05-20 00:19:29 +0800

    Add Editor Utilities

---
+++ Editor/Scripts/EditorUtilities.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace GrandoUniverse.Editor.Utilities {
+	
+	public static class EditorUtilities {
+		
+		public static T[] LoadAllDataFromFolder<T>(string _folderPath, string _filter = "") where T : UnityEngine.Object {
+			return AssetDatabase.FindAssets(_filter, new[] { _folderPath }).Select(val => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(val))).ToArray(); 
+		}
+		
+		public static Texture2D[] LoadAllTexture2DsFromFolder<T>(string _folderPath) {
+			return LoadAllDataFromFolder<Texture2D>(_folderPath, "t:Texture2D");
+		}
+		
+		public static Material[] LoadAllMaterialsFromFolder<T>(string _folderPath) {
+			return LoadAllDataFromFolder<Material>(_folderPath, "t:Material");
+		}
+		
+		public static GameObject[] LoadAllModelsFromFolder<T>(string _folderPath) {
+			return LoadAllDataFromFolder<GameObject>(_folderPath, "t:model");
+		}
+		
+	}
+
+}
